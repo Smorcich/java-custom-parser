3a6bfbbe88693157084e656067581eeac867f94a
Smorcich <52823878+Smorcich@users.noreply.github.com>
2020-12-01 20:45:23 +0300

    #2 scenebuilder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,23 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 import java.io.InputStream;
+import java.net.URL;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello world Application");
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(200);
-
-        /*InputStream iconStream = getClass().getResourceAsStream("/icon.png");
-        Image image = new Image(iconStream);
-        primaryStage.getIcons().add(image);
-         */
-
-        Label helloWorldLabel = new Label("Hello world!");
-        helloWorldLabel.setAlignment(Pos.CENTER);
-        Scene primaryScene = new Scene(helloWorldLabel);
-        primaryStage.setScene(primaryScene);
-
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/first.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
---
+++ src/main/java/MainSceneController.java
@@ -0,0 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainSceneController {
+
+    @FXML
+    private Button mainButton;
+
+    @FXML
+    private void buttonClicked() {
+        mainButton.setText("Click me again!");
+    }
+}
